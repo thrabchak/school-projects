42687963a91c9cf87e5c8e998c12c33fb14c2770
trh010 <trh010@bucknell.edu>
2013-09-19 20:48:51 -0400

    Updated menu bar: added new_notebook and new_note

--- Notebooks/gen/com/ctk/notebooks/R.java
+++ Notebooks/gen/com/ctk/notebooks/R.java
@@ -22,8 +22,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_new_note=0x7f020001;
+        public static final int ic_new_notebook=0x7f020002;
     }
     public static final class id {
+        public static final int action_new_note=0x7f080002;
+        public static final int action_new_notebook=0x7f080001;
         public static final int action_settings=0x7f080000;
     }
     public static final class layout {
@@ -36,6 +40,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int new_note=0x7f050003;
+        public static final int new_notebook=0x7f050004;
     }
     public static final class style {
         /** 
