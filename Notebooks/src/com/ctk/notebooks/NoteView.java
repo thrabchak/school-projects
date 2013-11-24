e0cd405c8870019ab0f48e4c09423022bf7bafd1
Tom Hrabchak <trh010@bucknell.edu>
2013-11-24 01:40:50 -0500

    hopefully added colors to slide_menu, along with linking color change to noteView.java

--- Notebooks/src/com/ctk/notebooks/NoteView.java
+++ Notebooks/src/com/ctk/notebooks/NoteView.java
@@ -12,29 +12,27 @@ import android.view.View;
 import android.widget.Toast;
 
 /**
- * Class extending from View that contains a Canvas on which
- * the user can draw.
- *
+ * Class extending from View that contains a Canvas on which the user can draw.
+ * 
  */
 public class NoteView extends View {
 
-	private Canvas	mCanvas;
-	private float 	mX, mY;
-	private Path	mPath;
-	private Bitmap 	mBitmap;
-	private Paint   mBitmapPaint;
-	private Paint	mPaint;
-	private int		mPaintColor = 0xFF000000;
-	private float	mPaintWidth	= 10;
-	private String 	fileName = "test1";
-	private boolean	mIsDrawingLocked = false;
-	
+	private Canvas			mCanvas;
+	private float			mX, mY;
+	private Path			mPath;
+	private Bitmap			mBitmap;
+	private Paint			mBitmapPaint;
+	private Paint			mPaint;
+	private int				mPaintColor			= 0xFF000000;
+	private float			mPaintWidth			= 10;
+	private final String	fileName			= "test1";
+	private boolean			mIsDrawingLocked	= false;
 
 	public NoteView(Context context) {
 		super(context);
 		init();
 	}
-	
+
 	public NoteView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		init();
@@ -49,10 +47,10 @@ public class NoteView extends View {
 	 * Initializes member variables. Called in every constructor above.
 	 */
 	private void init() {
-		
+
 		mBitmapPaint = new Paint(Paint.DITHER_FLAG);
 		mPath = new Path();
-		
+
 		// Create new Paint object, here we will be able
 		// to change the draw color and width.
 		mPaint = new Paint();
@@ -62,15 +60,17 @@ public class NoteView extends View {
 		mPaint.setAntiAlias(true);
 		mPaint.setDither(true);
 		this.setBackgroundColor(Color.TRANSPARENT);
-		
+
 	}
-	
+
 	/**
 	 * Sets the color of the paintbrush.
-	 * @param color	An integer in the form 0xaarrggbb, where <code>aa</code>
-	 * 				is the hex value for the color transparency, and
-	 * 				<code>rr</code>, <code>gg</code>, and <code>bb</code> are
-	 * 				the hex values respectively for red, green, and blue.
+	 * 
+	 * @param color
+	 *            An integer in the form 0xaarrggbb, where <code>aa</code> is
+	 *            the hex value for the color transparency, and <code>rr</code>,
+	 *            <code>gg</code>, and <code>bb</code> are the hex values
+	 *            respectively for red, green, and blue.
 	 * @return The current NoteView, to allow for method chaining.
 	 */
 	public NoteView setPaintColor(int color) {
@@ -78,10 +78,13 @@ public class NoteView extends View {
 		mPaint.setColor(mPaintColor);
 		return this;
 	}
-	
+
 	/**
 	 * Sets the width of the paintbrush.
-	 * @param width	A float for the width of the paintbrush (set to 10 by default).
+	 * 
+	 * @param width
+	 *            A float for the width of the paintbrush (set to 10 by
+	 *            default).
 	 * @return The current NoteView, to allow for method chaining.
 	 */
 	public NoteView setPaintWidth(float width) {
@@ -89,24 +92,26 @@ public class NoteView extends View {
 		mPaint.setStrokeWidth(mPaintWidth);
 		return this;
 	}
-	
+
 	/**
 	 * Sets whether drawing is currently enabled on the <code>NoteView</code>.
-	 * @param isLocked	<code>true</code> if the <code>NoteView</code> should be locked,
-	 * 					<code>false</code> if the <code>NoteView</code> should be drawn to.
+	 * 
+	 * @param isLocked
+	 *            <code>true</code> if the <code>NoteView</code> should be
+	 *            locked, <code>false</code> if the <code>NoteView</code> should
+	 *            be drawn to.
 	 */
 	public void setDrawingLocked(boolean isLocked) {
 		mIsDrawingLocked = isLocked;
 	}
 
 	/**
-	 * Called by the OS when the Canvas size is either
-	 * first set or changed.
+	 * Called by the OS when the Canvas size is either first set or changed.
 	 */
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
-		
+
 		if (mBitmap == null) {
 			mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
 			mCanvas = new Canvas(mBitmap);
@@ -115,33 +120,33 @@ public class NoteView extends View {
 			mCanvas = new Canvas(mBitmap);
 		}
 	}
-	
-	/**
-	 * Called by the OS to draw this View to the screen. It is here
-	 * where any drawing must be done. This method can be triggered
-	 * by calling <code>invalidate()</code>.
-	 */
-	@Override
-    protected void onDraw(Canvas canvas) {
-        canvas.drawColor(0xFFFFFFFF);
-        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
-        
-        if (mBitmap==null)
-        	Toast.makeText(getContext(), "no", Toast.LENGTH_SHORT).show();
-        
-        canvas.drawPath(mPath, mPaint);
-    }
 
 	/**
-	 * Called by the OS when a touch is registered. A MotionEvent is
-	 * passed by the OS, from which it can be determined the type of
-	 * action [ACTION_DOWN, ACTION_MOVE, ACTION_UP, among others].
+	 * Called by the OS to draw this View to the screen. It is here where any
+	 * drawing must be done. This method can be triggered by calling
+	 * <code>invalidate()</code>.
+	 */
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.drawColor(0xFFFFFFFF);
+		canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+
+		if (mBitmap == null)
+			Toast.makeText(getContext(), "no", Toast.LENGTH_SHORT).show();
+
+		canvas.drawPath(mPath, mPaint);
+	}
+
+	/**
+	 * Called by the OS when a touch is registered. A MotionEvent is passed by
+	 * the OS, from which it can be determined the type of action [ACTION_DOWN,
+	 * ACTION_MOVE, ACTION_UP, among others].
 	 */
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		float x = event.getX();
 		float y = event.getY();
-		
+
 		if (!mIsDrawingLocked) {
 			switch (event.getAction()) {
 			case MotionEvent.ACTION_DOWN:
@@ -153,44 +158,49 @@ public class NoteView extends View {
 				return true;
 			case MotionEvent.ACTION_MOVE:
 				float dx = Math.abs(x - mX);
-	            float dy = Math.abs(y - mY);
-	            if (dx >= 4 || dy >= 4) {
-	                mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
-	                mX = x;
-	                mY = y;
-	            }
-	            invalidate();
-	            return true;
+				float dy = Math.abs(y - mY);
+				if (dx >= 4 || dy >= 4) {
+					mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+					mX = x;
+					mY = y;
+				}
+				invalidate();
+				return true;
 			case MotionEvent.ACTION_UP:
 				mPath.lineTo(mX, mY);
-	            mCanvas.drawPath(mPath, mPaint);
-	            mPath.reset();
-	            invalidate();
-	            return true;
+				mCanvas.drawPath(mPath, mPaint);
+				mPath.reset();
+				invalidate();
+				return true;
 			}
 		}
 		return true;
 	}
-	
+
 	/**
-	 * @return	The underlying <code>Bitmap</code> image of the NoteView's <code>Canvas</code>.
+	 * @return The underlying <code>Bitmap</code> image of the NoteView's
+	 *         <code>Canvas</code>.
 	 */
-	public Bitmap getBitmap(){
+	public Bitmap getBitmap() {
 		return mBitmap;
 	}
-	
+
 	/**
-	 * @return	The filename given to the current sheet of notes.
+	 * @return The filename given to the current sheet of notes.
 	 */
 	public String getFileName() {
 		return fileName;
 	}
-	
+
 	/**
-	 * Sets the <code>Bitmap</code> image of <code>Canvas</code> of the <code>NoteView</code>.
-	 * @param bitmap	The <code>Bitmap</code> that will become the background of the <code>NoteView</code>
+	 * Sets the <code>Bitmap</code> image of <code>Canvas</code> of the
+	 * <code>NoteView</code>.
+	 * 
+	 * @param bitmap
+	 *            The <code>Bitmap</code> that will become the background of the
+	 *            <code>NoteView</code>
 	 */
-	public void setBitmap(Bitmap bitmap){
+	public void setBitmap(Bitmap bitmap) {
 		mBitmap = bitmap;
 	}
 }
