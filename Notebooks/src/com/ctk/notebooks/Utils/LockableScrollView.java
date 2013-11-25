5168ed3c3684bbbcb35d8020bbb623db26b896b1
Tom Hrabchak <trh010@bucknell.edu>
2013-11-24 20:31:29 -0500

    Tried to work on scrolling, but didn't get anywhere. Time to look at multi-touch gestures http://developer.android.com/training/gestures/multi.html

--- Notebooks/src/com/ctk/notebooks/Utils/LockableScrollView.java
+++ Notebooks/src/com/ctk/notebooks/Utils/LockableScrollView.java
@@ -7,13 +7,13 @@ import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class LockableScrollView extends ScrollView {
-	
-	private boolean mIsScrollLocked = false;
 
-    public LockableScrollView(Context context) {
+	private boolean	mIsScrollLocked	= false;
+
+	public LockableScrollView(Context context) {
 		super(context);
 	}
-    
+
 	public LockableScrollView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
@@ -24,32 +24,58 @@ public class LockableScrollView extends ScrollView {
 
 	/**
 	 * Toggle whether the <code>LockableScrollView</code> can be scrolled.
-	 * @param isScrollLocked	Pass <code>true</code> if scrolling should be locked,
-	 * 							<code>false</code> otherwise.
+	 * 
+	 * @param isScrollLocked
+	 *            Pass <code>true</code> if scrolling should be locked,
+	 *            <code>false</code> otherwise.
 	 */
-    public void setScrollingLocked(boolean isScrollLocked) {
-        mIsScrollLocked = isScrollLocked;
-    }
+	public void setScrollingLocked(boolean isScrollLocked) {
+		mIsScrollLocked = isScrollLocked;
+	}
 
-    /**
-     * @return	<code>true</code> if the <code>LockableScrollView</code> is scrollable,
-     * 			<code>false</code> otherwise.
-     */
-    public boolean isScrollLocked() {
-        return mIsScrollLocked;
-    }
+	/**
+	 * @return <code>true</code> if the <code>LockableScrollView</code> is
+	 *         scrollable, <code>false</code> otherwise.
+	 */
+	public boolean isScrollLocked() {
+		return mIsScrollLocked;
+	}
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent event) {
-//    	Toast.makeText(getContext(), "ptrs = " + event.getPointerCount(), Toast.LENGTH_SHORT).show();
-    	
-    	if(event.getPointerCount() == 2){
-//    		Toast.makeText(getContext(), "scroll loc = " + getScrollY(), Toast.LENGTH_SHORT).show();
-//    		smoothScrollBy(0, # amount to scroll by); //work here
-    		return super.onInterceptTouchEvent(event);
-    	} else if (!mIsScrollLocked){ 
-        	return super.onInterceptTouchEvent(event);
-    	} else 
-        	return false;
-    }
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+		return true;
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent event) {
+		// Toast.makeText(getContext(), "ptrs = " + event.getPointerCount(),
+		// Toast.LENGTH_SHORT).show();
+		Toast.makeText(getContext(), event.toString(), Toast.LENGTH_SHORT)
+				.show();
+		if (event.getPointerCount() == 2) {
+
+			if (mY == -1) {
+				mY = event.getY();
+			} else {
+				float newY = event.getY();
+				float dy = newY - mY;
+				smoothScrollBy(0, (int) dy);
+				mY = newY;
+			}
+			return true;
+			// Toast.makeText(getContext(), "scroll loc = " + getScrollY(),
+			// Toast.LENGTH_SHORT).show();
+			// smoothScrollBy(0, # amount to scroll by); //work here
+			// return super.onInterceptTouchEvent(event);
+		} else if (!mIsScrollLocked) {
+			mY = -1;
+			return super.onInterceptTouchEvent(event);
+		} else {
+			mY = -1;
+			return false;
+		}
+
+	}
+
+	private float	mY	= -1;
 }
