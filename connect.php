27f8421ef1196e48f309f3d821cf4bbf68ce65ae
kmw023 <kmw023@bucknell.edu>
2013-12-08 15:23:51 -0500

    Added the two necessary files. Working now.

---
+++ connect.php
@@ -0,0 +1,30 @@
+<?php 
+  $user = "kmw023";
+  $password = "iexeipiM";
+  $database = "test";
+  $conn = mysql_connect("db.eg.bucknell.edu",$user,$password);
+  if (!$conn) 
+     die("Cound not connect to database");
+  
+  mysql_select_db($database) or die("Unable to select database");
+
+  $result = mysql_query("SHOW TABLES");
+  if (!$result) 
+    die("Query to show tables failed");
+
+  $num_row = mysql_num_rows($result);
+  
+  echo "<h1>Choose one table:</h1>";
+  echo "<form action=\"showtable.php\" method=\"POST\">";
+  echo "<select name=\"table\" size=\"1\" Font size=\"2\">";
+  for($i = 0; $i < $num_row; $i++) {
+      $tablename = mysql_fetch_row($result);
+      echo "<option value = \"{$tablename[0]}\" >{$tablename[0]}</option>";
+  }
+  echo "</select>";
+  echo "<div><input type=\"submit\" value=\"submit\"></div>";
+  echo "</form>";
+  
+  mysql_free_result($result);
+  mysql_close($conn);
+?>
