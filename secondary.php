404327e328f2e23fd4ec470127fc1b0b46f24096
trh010 <trh010@bucknell.edu>
2013-12-10 01:02:01 -0500

    more updates, just using test.html and secondary.php now

--- secondary.php
+++ secondary.php
@@ -1,29 +1,44 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="./icon/icon.jpg">
-    <!-- Bootstrap core CSS -->
-    <link href="./css/bootstrap.css" rel="stylesheet">
-    <!-- Bootstrap theme -->
-    <link href="./css/bootstrap-theme.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-     <link href="./css/navbar.css" rel="stylesheet">
+	<head>
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <meta name="description" content="">
+	  <meta name="author" content="">
+	  <link rel="shortcut icon" href="./icon/icon.jpg">
+	  <link href="./css/bootstrap.css" rel="stylesheet">
+	  <link href="./css/bootstrap-theme.min.css" rel="stylesheet">
+	  <link href="./css/navbar.css" rel="stylesheet">
 
-    <title>Grocery Store</title>
+  <title>Grocery Store</title>
 	</head>
 
 	<body>
 		<!-- Static navbar -->
       <div class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">CS305 - Grocery Store</a>
+      	<div class="navbar-header">
+      		<a class="navbar-brand" href="test.html">CS305 - Grocery Store</a>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
         </div>
         <div class="navbar-collapse collapse">
+        	<!-- <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Change View<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+              	<li><a href="secondary.php">Admin</a></li>
+                <li><a href="secondary.php">Customer</a></li>
+                <li><a href="secondary.php">Manager</a></li>
+                <li><a href="secondary.php">Cashier</a></li>
+                <li><a href="secondary.php">Stocker</a></li>
+              </ul>
+            </li>
+          </ul> -->
           <ul class="nav navbar-nav navbar-right">
             <li><a href="http://www.eg.bucknell.edu/~trh010/cs305/test.html">Login Page</a></li>
           </ul>
@@ -46,9 +61,36 @@
 
 				switch ($_POST["job"]) {
 			    case "admin":
+			?>
+			<h2>Customers</h2>
+			<?php
+			    	$result = mysql_query("SELECT * FROM Customers INNER JOIN CustomerInfo ON Customers.CustomerID=CustomerInfo.CustomerID;");
+					  if (!$result) 
+					    die("Query to show tuples from table failed!" . mysql_error());
+
+					  $fields_num = mysql_num_fields($result);
+					  echo "<table class=\"table table-hover\" ><thead><tr>";
+					  // Print headers
+					  for($i = 0; $i < $fields_num; $i++) {
+					      $field = mysql_fetch_field($result);
+					      echo "<th>{$field->name}</th>";
+					  }
+					  echo "</tr></thead>\n<tbody>\n";
+					  
+					  // Print data
+					  while($row = mysql_fetch_row($result)) {
+					      echo "<tr>";
+					      foreach($row as $cell)
+					          echo "<td>$cell</td>";
+					      echo "</tr>\n";
+					  }
+					  echo "</tbody>\n</table>";
+					  mysql_free_result($result);
 
 			    case "manager":
-			    	echo "<h2>Employees</h2>";
+			?>
+			<h2>Employees</h2>
+			<?php
 			    	$result = mysql_query("SELECT * FROM Employees");
 					  if (!$result) 
 					    die("Query to show tuples from table failed!" . mysql_error());
@@ -73,7 +115,34 @@
 					  mysql_free_result($result);
 					  
 			    case "cashier":
-			    	echo "<h2>Checkout Log</h2>";
+			?>
+			<h2>Carts</h2>
+			<?php 
+			    	$result = mysql_query("SELECT * FROM Carts");
+					  if (!$result) 
+					    die("Query to show tuples from table failed!" . mysql_error());
+
+					  $fields_num = mysql_num_fields($result);
+					  echo "<table class=\"table table-hover\" ><thead><tr>";
+					  // Print headers
+					  for($i = 0; $i < $fields_num; $i++) {
+					      $field = mysql_fetch_field($result);
+					      echo "<th>{$field->name}</th>";
+					  }
+					  echo "</tr></thead>\n<tbody>\n";
+					  
+					  // Print data
+					  while($row = mysql_fetch_row($result)) {
+					      echo "<tr>";
+					      foreach($row as $cell)
+					          echo "<td>$cell</td>";
+					      echo "</tr>\n";
+					  }
+					  echo "</tbody>\n</table>";
+					  mysql_free_result($result);
+			?>
+			<h2>Checkout Log</h2>
+			<?php
 			    	$result = mysql_query("SELECT * FROM CheckedOut");
 					  if (!$result) 
 					    die("Query to show tuples from table failed!" . mysql_error());
@@ -98,8 +167,10 @@
 					  mysql_free_result($result);
 
 			    case "stocker":
-		    		echo "<h2>Items Available</h2>";
-		    		$result = mysql_query("SELECT * FROM GroceryShelves");
+  		?>
+  		<h2>Items In Inventory</h2>
+  		<?php
+		    		$result = mysql_query("SELECT * FROM StockShelves");
 					  if (!$result) 
 					    die("Query to show tuples from table failed!" . mysql_error());
 
@@ -123,8 +194,10 @@
 					  mysql_free_result($result);
 			   
 			    default: # Customer
-		        echo "<h2>Items</h2>";
-		        $result = mysql_query("SELECT * FROM ItemList");
+	    ?>
+	    <h2>Items On Shelves</h2>
+	    <?php
+		    		$result = mysql_query("SELECT * FROM GroceryShelves INNER JOIN ItemList ON GroceryShelves.item=ItemList.Item;");
 					  if (!$result) 
 					    die("Query to show tuples from table failed!" . mysql_error());
 
