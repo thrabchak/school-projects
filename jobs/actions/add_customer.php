ed1be2d049b889dae9f44287d277df907a240ac9
kmw023 <kmw023@bucknell.edu>
2013-12-10 18:52:41 -0500

    Finished customers menus

--- jobs/actions/add_customer.php
+++ jobs/actions/add_customer.php
@@ -12,7 +12,7 @@
 	echo $name;
 	echo $address;
 	*/
-	
+
 	// Get id num
 	$result = mysql_query("SELECT MAX(customerID) FROM Customers;");
 	if (!$result) 
@@ -27,9 +27,31 @@
 	$row = mysql_fetch_row($result);
 	$cartnum = $row[0] + 1;
 
+	// Compare name
+	$matched = 0;
+	$result = mysql_query("SELECT customerName, customerID FROM CustomerInfo;");
+	if (!$result) 
+	  die("Query to show tuples from table failed!" . mysql_error());
+	$num_row = mysql_num_rows($result);
+
+	for($i = 0; $i < $num_row; $i++) {
+		$tablename = mysql_fetch_row($result);
+		if ($tablename[0] == $name){
+			$idnum = $tablename[1];
+			$matched = 1;
+		}
+	}
+
 	// Create new table value
-	$result = mysql_query("INSERT INTO CustomerInfo VALUES('$idnum', '$name', '$address');");
+	if ($matched == 0){
+		$result = mysql_query("INSERT INTO CustomerInfo VALUES('$idnum', '$name', '$address');");
+		if (!$result) 
+          		die("Query failed!" . mysql_error());
+	}
+
 	$result = mysql_query("INSERT INTO Customers VALUES('$cartnum', '$idnum');");
+	if (!$result) 
+          die("Query failed!" . mysql_error());
 	
 	$url = "http://eg.bucknell.edu/~kmw023/cs305/jobs/customer.php";
 	while (ob_get_status()) 
--- jobs/actions/add_item.php
+++ jobs/actions/add_item.php
@@ -5,8 +5,8 @@
         if (!$conn) die("Cound not connect to database");       
         mysql_select_db($_SESSION["database"]) or die("Unable to select database");
 
-	$name = $_POST['cart'];
-	$address = $_POST['item'];
+	$idnum = $_POST['cart'];
+	$item = $_POST['item'];
 	$quantity = $_POST['quantity'];
 
 	/*
@@ -15,9 +15,50 @@
 	echo $quantity
 	*/
 	
-	// Create new table value
-	$result = mysql_query("INSERT INTO Carts VALUES('$idnum', '$item', '$quantity');");
+	// Check to see if the item is availiable
+	$result = mysql_query("SELECT count FROM GroceryShelves WHERE item='$item';");
+	if (!$result) 
+	  die("Query (1) to show tuples from table failed!" . mysql_error());
+	$row = mysql_fetch_row($result);
+
+	if ($row[0] > $quantity){ // Reduce count on grocery shelves
+		$temp = $row[0] - $quantity;
+		$result = mysql_query("UPDATE GroceryShelves SET count=$temp WHERE item='$item';");
+		if (!$result) 
+	  		die("Query (2) to show tuples from table failed!" . mysql_error());
+	}
+	else { // $row[0] <= $quantity // remove item from grocery shelves
+		$quantity = $row[0];
+		$result = mysql_query("DELETE FROM GroceryShelves WHERE item='$item';");
+		if (!$result) 
+	  		die("Query (3) to show tuples from table failed!" . mysql_error());
+	}
 	
+
+	// Check to see if the item is already in cart
+	$result = mysql_query("SELECT EXISTS(SELECT 1 FROM Carts WHERE item='$item' AND cartID=$idnum);");
+	if (!$result) 
+	  die("Query (4) to show tuples from table failed!" . mysql_error());
+	$row = mysql_fetch_row($result);
+
+	if ($row[0] == 1){ // Item exists, add to its value
+		$result = mysql_query("SELECT itemQuantity FROM Carts WHERE item='$item' AND cartID=$idnum;");
+		if (!$result) 
+	  		die("Query (5) to show tuples from table failed!" . mysql_error());
+		$oldvalue = mysql_fetch_row($result);
+
+		$value = $oldvalue[0] + $quantity;
+		$result = mysql_query("UPDATE Carts SET itemQuantity=$value WHERE item='$item' AND cartID=$idnum;");
+		if (!$result) 
+	  		die("Query (6) to show tuples from table failed!" . mysql_error());
+	}
+	else{ // Item doesnt exist, make a new table entry
+		// Create new table value
+		$result = mysql_query("INSERT INTO Carts VALUES('$idnum', '$item', '$quantity');");
+		if (!$result) 
+	  		die("Query (7) to show tuples from table failed!" . mysql_error());
+	}
+
 	$url = "http://eg.bucknell.edu/~kmw023/cs305/jobs/customer.php";
 	while (ob_get_status()) 
 	{
