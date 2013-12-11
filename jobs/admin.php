1bf1423d7a7b24db90434f6de6e985be9182d500
trh010 <trh010@bucknell.edu>
2013-12-11 02:30:42 -0500

    added note

--- jobs/admin.php
+++ jobs/admin.php
@@ -169,6 +169,7 @@
         mysql_free_result($result);
       ?>
       <h2>Checkout Log</h2>
+      <p><b>note: </b>the cartID is the second column</p>
       <?php
         $result = mysql_query("SELECT * FROM CheckedOut ORDER BY cartID ASC;");
         if (!$result) 
