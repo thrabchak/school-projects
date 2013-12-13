3a708ee35a36485193c0f887ce5d299a215492a2
trh010 <trh010@bucknell.edu>
2013-12-13 11:19:29 -0500

    fixed bug on stock_shelves

--- jobs/actions/stock_shelves.php
+++ jobs/actions/stock_shelves.php
@@ -13,6 +13,11 @@
 	if (!$result) die("Query1 failed!" . mysql_error());
 	$row = mysql_fetch_row($result);
 	$num_in_stock = intval($row[0]);
+
+	$result = mysql_query("SELECT DISTINCT count FROM GroceryShelves WHERE item='$item';");
+	if (!$result) die("Query1 failed!" . mysql_error());
+	$row = mysql_fetch_row($result);
+	$num_on_shelves = intval($row[0]);
 	//echo "<br>".$item . "<br>" . $quantity . "<br>" . $num_in_stock . "<br>";
 	
 
@@ -20,7 +25,7 @@
 	if($num_in_stock > $quantity and $num_in_stock > 0){
 
 		// update GroceryShelves
-		$new_onShelves_quant = $num_in_stock + $quantity;
+		$new_onShelves_quant = $num_on_shelves + $quantity;
 		//echo $new_onShelves_quant . "<br>";
 		$result = mysql_query("UPDATE GroceryShelves SET GroceryShelves.count=($new_onShelves_quant) WHERE item='$item';");
 		if (!$result) die("Query2 failed!" . mysql_error());
